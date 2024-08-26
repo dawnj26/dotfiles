b3a55652a9bc21d3ebc08768c0785c853ec70753
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-08-26 14:52:52 +0800

    update deps

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -4,10 +4,12 @@
 --
 
 if vim.g.neovide then
-  vim.o.guifont = "JetBrainsMono Nerd Font:h13"
+  vim.o.guifont = "GohuFont 14 Nerd Font:h12"
   vim.g.neovide_cursor_vfx_mode = "ripple"
+  vim.g.transparency = 0.8
+  vim.g.neovide_transparency = 0.8
 
   vim.g.neovide_input_use_logo = 1
-  vim.api.nvim_set_keymap("!", "<C-v>", "<C-R>+", { noremap = true, silent = true })
-  vim.api.nvim_set_keymap("t", "<C-v>", "<C-R>+", { noremap = true, silent = true })
+  -- vim.api.nvim_set_keymap("!", "<C-v>", "<C-R>+", { noremap = true, silent = true })
+  -- vim.api.nvim_set_keymap("t", "<C-v>", "<C-R>+", { noremap = true, silent = true })
 end
