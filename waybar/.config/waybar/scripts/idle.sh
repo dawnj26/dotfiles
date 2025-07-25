77c1cbc4b3c07c1a791f8c115754e1f7fcbe9913
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2025-07-25 14:23:06 +0800

    new waybar

--- waybar/.config/waybar/scripts/idle.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ $(pidof hypridle) ]]; then
-  echo ""
-else
-  echo ""
-fi
--- waybar/.config/waybar/scripts/switch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ $(pidof hypridle) ]]; then
-  pkill hypridle
-  notify-send "Hypridle is disabled"
-else
-  hypridle &
-  notify-send "Hypridle is enabled"
-fi
